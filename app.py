e441250232b7c2175b4977898ea6854e2f873b34
capt-alien <eric.botcher@presidio.edu>
2019-01-31 13:29:14 -0800

    html

--- app.py
+++ app.py
@@ -18,5 +18,36 @@ def home():
     world = "Hello World"
     return render_template("main.html", market = world)
 
+#Model
+market = {
+    'market_id': 'M123',
+    'name': 'Alamany Farmers Market',
+    'location': '100 Alemany Blvd, San Francisco, CA 94110',
+}
+
+# INDEX
+@app.route('/markets')
+def index():
+    # name = market["name"]
+    # location = market["location"]
+    return render_template("markets.html", name=market["name"], location=market["location"])
+
+class Market(Resource):
+
+    def get(self, name):
+        pass
+
+    def post(self, name):
+        pass
+
+    def put(self, name):
+        pass
+
+    def delete(self, name):
+        pass
+        
+
+
+
 
 app.run(port = 5000, debug=True)
